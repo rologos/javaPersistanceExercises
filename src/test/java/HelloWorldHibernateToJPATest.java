6a62a1977cbf54aad80c5eb69b722f37a5c47471
rologos <170283129+rologos@users.noreply.github.com>
2024-10-23 17:06:43 -0500

    finalize hibernate to jpa test

--- src/test/java/HelloWorldHibernateToJPATest.java
+++ src/test/java/HelloWorldHibernateToJPATest.java
@@ -27,4 +27,45 @@ public class HelloWorldHibernateToJPATest {
         }
         return Persistence.createEntityManagerFactory("ch02",properties);
     }
+
+    @Test
+    public void storeLoadMessage() {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("ch02");
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            Message message = new Message();
+            message.setText("Hello World!");
+
+            em.persist(message);
+
+            em.getTransaction().commit();
+            //INSERT into MESSAGE (ID, TEXT) values (1, 'Hello World!')
+
+            em.getTransaction().begin();
+
+            List<Message> messages =
+                    em.createQuery("select m from Message m", Message.class).getResultList();
+            //SELECT * from MESSAGE
+
+            messages.get(messages.size() - 1).setText("Hello World from Hibernate to JPA!");
+
+            em.getTransaction().commit();
+            //UPDATE MESSAGE set TEXT = 'Hello World from JPA!' where ID = 1
+
+            assertAll(
+                    () -> assertEquals(1, messages.size()),
+                    () -> assertEquals("Hello World from Hibernate to JPA!", messages.get(0).getText())
+            );
+
+            em.close();
+
+        } finally {
+            emf.close();
+        }
+    }
 }
